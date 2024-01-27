dbd63d5a4a448d627744b09069869e291d843586
LiuzxDaniel <157857941+LiuzxDaniel@users.noreply.github.com>
2024-01-26 22:16:01 -0500

    Update clicker.py

--- clicker.py
+++ clicker.py
@@ -2,7 +2,7 @@ import time, random
 from pynput import keyboard
 from pynput.mouse import Controller, Button
 
-hotkey = "r"
+hotkey = 'r'
 press_key = Button.left
 max_delay = 0.1
 min_delay = 0.11
@@ -10,11 +10,14 @@ min_delay = 0.11
 mouse = Controller()
 
 def on_release(key):
-    if key.char == hotkey:
-        while True:
-            mouse.press(press_key)
-            mouse.release(press_key)
-            time.sleep(round(random.uniform(max_delay, min_delay), 4))
+    try:
+        if key.char == hotkey:
+            while True:
+                mouse.press(press_key)
+                mouse.release(press_key)
+                time.sleep(round(random.uniform(max_delay, min_delay), 4))
+    except AttributeError:
+        pass
 
 
 with keyboard.Listener(on_release=on_release) as listener:
